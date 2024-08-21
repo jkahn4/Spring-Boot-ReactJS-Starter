5e1de9fbd981585f1fe9467fc4d1c91b6c086072
Joshua Kahn <joshua.kahn4@gmail.com>
2024-08-21 10:35:44 -0700

    Test

---
+++ spring-react-combo-app/src/main/java/e/the/awesome/spring_react_combo_app/SpringReactComboAppApplication.java
@@ -0,0 +1,13 @@
+package e.the.awesome.spring_react_combo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactComboAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactComboAppApplication.class, args);
+	}
+
+}
---
+++ spring-react-combo-app/src/test/java/e/the/awesome/spring_react_combo_app/SpringReactComboAppApplicationTests.java
@@ -0,0 +1,13 @@
+package e.the.awesome.spring_react_combo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactComboAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
